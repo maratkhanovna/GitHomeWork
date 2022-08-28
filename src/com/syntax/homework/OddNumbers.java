0290cd1512194319d0746c0c680b85f28c160098
maratkhanovna <maratkhanovna@gmail.com>
2022-08-27 23:13:32 -0400

    code to display odd numbers from 1 to 100

---
+++ src/com/syntax/homework/OddNumbers.java
@@ -0,0 +1,12 @@
+package com.syntax.homework;
+
+public class OddNumbers {
+    public static void main(String[] args) {
+        int num = 100;
+        for (int i=1; i<num; i++){
+            if (i%2!=0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
