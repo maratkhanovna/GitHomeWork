9e980d6c4834302b3107c617ba552e09f3f951d7
maratkhanovna <maratkhanovna@gmail.com>
2022-08-27 23:59:10 -0400

    modified code to display even numbers

--- src/com/syntax/homework/OddNumbers.java
+++ src/com/syntax/homework/OddNumbers.java
@@ -4,7 +4,7 @@ public class OddNumbers {
     public static void main(String[] args) {
         int num = 100;
         for (int i=1; i<num; i++){
-            if (i%2!=0){
+            if (i%2==0){
                 System.out.print(i+" ");
             }
         }
